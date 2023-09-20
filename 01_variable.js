c580fb134fed12b88b64e5e99cd14ae06e13204f
PriyaMahalle <priyaakhare88@gmail.com>
2023-09-20 09:25:19 +0530

    variable assignment
    variable assignment

---
+++ 01_variable.js
@@ -0,0 +1,20 @@
+
+var firstName; // variable declaration
+firstName = "Priya";  // Variable initialization
+console.log(firstName);
+firstName = "Mrs. Priya"; // Variable update
+console.log(firstName);
+
+
+var lastName = "Mahalle"; // variable declaration and initialization
+console.log(lastName);
+
+var height; // Variable Declaration
+console.log(height);
+height = "5"; // initialization
+console.log(height);
+height = "5.5 feet";// update
+console.log(height);
+
+//var marks= '80%';
+//console.log("10th marks:" marks);
---
+++ 01_variableAssign.js
@@ -0,0 +1,25 @@
+var myFullName;
+console.log(myFullName);
+
+
+var firstName, lastName;
+firstName = 'Priya';
+lastName = 'Mahalle';
+console.log(firstName, lastName);
+
+var myFullName;
+myFullName = "Priya Sagar Mahalle";
+console.log(myFullName);
+
+var myLove;
+myLove = 'I Love only JS';
+console.log(myLove);
+
+var firstName='Priya', lastName='Mahalle', age='26', address='Akola', pincode='444501', gender='Female', ismarried='YES';
+console.log("First Name:", firstName);
+console.log("Last Name:", lastName);
+console.log("Age:", age);
+console.log("Address:", address);
+console.log("Pincode:", pincode);
+console.log("Gender:", gender);
+console.log("Ismarried:", ismarried);
