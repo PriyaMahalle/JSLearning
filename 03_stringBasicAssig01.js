09cd662ebb0417e6ffd1f1edbcdd6f05eefc0444
PriyaMahalle <priyaakhare88@gmail.com>
2023-09-25 14:42:14 +0530

    Assignment using String
    Assignment using String

---
+++ 03_stringBasicAssig01.js
@@ -0,0 +1,17 @@
+console.log('************************* Assignment no- 2 *********************');
+console.log('================== Step- 1 ===================');
+function stringBasics() {
+    console.log('My Dream Company is TCS');
+}
+stringBasics()
+
+console.log('=================== Step- 2 ==================');
+var hobby1= 'Sketching';
+var hobby2= 'Cooking';
+var hobby3= 'Travelling';
+console.log("Hobby 1:", hobby1, "Hobby 2:", hobby2, "Hobby 3:", hobby3);
+
+console.log('=================== Step- 3 ==================');
+var hobbies= 'Sketching, Cooking2, Travelling3';
+var summation= hobbies.length;
+console.log("Sum of total number of character is: ", summation);
