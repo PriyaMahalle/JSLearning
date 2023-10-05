a5cf3f17feca563bae2268d1f31dcea576130a52
PriyaMahalle <priyaakhare88@gmail.com>
2023-10-05 20:03:25 +0530

    scope of variables
    scope of variables

--- 03_countCharacter.js
+++ 03_countCharacter.js
@@ -1,4 +1,4 @@
-console.log(`************* Assignment No- To count character 'a' or 'A'************`);
+console.log(`************* Assignment- To count character 'a' or 'A'************`);
 
 function countChar(str) {
     var count = 0;
@@ -12,7 +12,7 @@ console.log(`Total number of "a" and "A" in given string are: ${count}`);
 }
 console.log('Given string is: "I Am learning JavaScript, The language of internet" ');
 countChar("I Am learning JavaScript, The language of internet")
-console.log(`========================================================================`);
+console.log(`=======================================================================`);
 console.log('Given string is: "My favourite movie is LAggAn"');
 countChar("My favourite movie is LAggAn")
 
