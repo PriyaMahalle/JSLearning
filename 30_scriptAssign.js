de022bc119f427d8caad7a7959e8070760ceb796
PriyaMahalle <priyaakhare88@gmail.com>
2023-10-25 15:19:24 +0530

    DOM methods
    DOM methods

---
+++ 30_scriptAssign.js
@@ -0,0 +1,8 @@
+const elementHeading = document.getElementById('overview');
+console.log(elementHeading.innerHTML);
+
+const elementList = document.getElementById('available');
+console.log(elementList.innerHTML);
+
+const elementAllList = document.getElementById('advantages');
+console.log(elementAllList.innerHTML);
